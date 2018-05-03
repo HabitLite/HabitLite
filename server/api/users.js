e298a6e226a2afc53db577b843aaf2790103b71e
Priya <banspri0@gmail.com>
2018-05-03 12:09:56 -0400

    Write following tests for user route: -GET /api/users -GET /api/users/:userId
    Still need to write following tests for user route:
    -POST /api/users/:userId
    -PUT /api/users/:userId
    -DELETE /api/users/:userId

--- server/api/users.js
+++ server/api/users.js
@@ -4,11 +4,22 @@ module.exports = router
 
 router.get('/', (req, res, next) => {
   User.findAll({
-    // explicitly select only the id and email fields - even though
+    // explicitly select only the id and username fields - even though
     // users' passwords are encrypted, it won't help if we just
     // send everything to anyone who asks!
-    attributes: ['id', 'email']
+    attributes: ['id', 'username']
   })
     .then(users => res.json(users))
+    .catch(next)
+})
+
+router.get('/:userId', (req, res, next) => {
+  User.findOne({
+    where: {
+      id: req.params.userId
+    },
+    attributes: ['id', 'username']
+  })
+    .then(user => res.json(user))
     .catch(next)
 })
--- server/api/users.spec.js
+++ server/api/users.spec.js
@@ -6,28 +6,85 @@ const db = require('../db')
 const app = require('../index')
 const User = db.model('user')
 
-xdescribe('User routes', () => {
+describe('User routes', function () {
+
   beforeEach(() => {
     return db.sync({force: true})
   })
 
-  describe('/api/users/', () => {
-    const codysEmail = 'cody@puppybook.com'
+  describe('/api/users/', function () {
+    const codysUsername = 'codylovesdogs'
 
     beforeEach(() => {
       return User.create({
-        email: codysEmail
+        username: codysUsername
       })
     })
 
-    it('GET /api/users', () => {
-      return request(app)
-        .get('/api/users')
-        .expect(200)
-        .then(res => {
-          expect(res.body).to.be.an('array')
-          expect(res.body[0].email).to.be.equal(codysEmail)
-        })
-    })
+    describe('GET /api/users', function () {
+
+      it('sends a res.body of all users in database', function () {
+        return request(app)
+          .get('/api/users')
+          .expect(200)
+          .then(res => {
+            expect(res.body).to.be.an('array')
+            expect(res.body[0].username).to.be.equal(codysUsername)
+          })
+      })
+
+      it('does not include users\' passwords in res.body', function () {
+        return request(app)
+          .get('/api/users')
+          .expect(200)
+          .then(res => {
+            expect(res.body).to.be.an('array')
+            expect(res.body[0].password).to.be.equal(undefined)
+          })
+      })
+
+    }) // end describe ('GET /api/users')
+
   }) // end describe('/api/users')
+
+  describe('/api/users/:userId', function () {
+
+    beforeEach(() => {
+      return User.bulkCreate([{
+        username: 'codylovesdoggos'
+      }, {
+        username: 'puppylover369'
+      }, {
+        username: 'daisylover246'
+      }, {
+        username: 'doggidydog'
+      }])
+    })
+
+    describe('GET /api/users/:userId', function () {
+      it('sends a res.body of specific user in database', function () {
+        return request(app)
+          .get('/api/users/4')
+          .expect(200)
+          .then(res => {
+            expect(res.body).to.be.an('object')
+            expect(res.body.username).to.be.equal('doggidydog')
+          })
+      })
+      it('does not include user\'s password in res.body', function () {
+        return request(app)
+          .get('/api/users/1')
+          .expect(200)
+          .then(res => {
+            expect(res.body).to.be.an('object')
+            expect(res.body.password).to.be.equal(undefined)
+          })
+      })
+    }) // end describe('GET /api/users/:userId')
+  }) // end describe('/api/users/:userId')
 }) // end describe('User routes')
+
+//Specific user
+//Post a user
+//Update a user
+//Remove a user
