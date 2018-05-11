229f50f44057b9e55c75738d1fb8c6baeae8ab82
Tania Santamaria <tsantamaria.r@gmail.com>
2018-05-10 22:14:12 -0400

    Continue working on Personality Quiz P2

--- client/components/Personality.js
+++ client/components/Personality.js
@@ -1,14 +1,5 @@
 import React, { Component } from 'react';
-import SelectField from 'material-ui/SelectField';
-import MenuItem from 'material-ui/MenuItem';
-import { List, ListItem } from 'material-ui/List';
-import Subheader from 'material-ui/Subheader';
-import Divider from 'material-ui/Divider';
-import Checkbox from 'material-ui/Checkbox';
-import Toggle from 'material-ui/Toggle';
 import { RadioButton, RadioButtonGroup } from 'material-ui/RadioButton';
-import ActionFavorite from 'material-ui/svg-icons/action/favorite';
-import ActionFavoriteBorder from 'material-ui/svg-icons/action/favorite-border';
 import RaisedButton from 'material-ui/RaisedButton';
 import axios from 'axios'
 import quiz from '../../script/quiz.js'
@@ -35,6 +26,7 @@ export default class Personality extends Component {
             q9: '',
             q10: '',
             q11: '',
+            insight: ''
         }
     }
 
@@ -45,11 +37,13 @@ export default class Personality extends Component {
 
     render() {
         console.log("inside personality and this is the STATE ", this.state)
+
         return (
             <div>
                 <form onSubmit={this.handleSubmit} className='form'>
                     {
                         quiz.map((quizContent, idx) => {
+                            this.state.insight + quizContent.question
                             return (
                                 <span key={idx}>
 
@@ -66,6 +60,7 @@ export default class Personality extends Component {
                                                 />
 
                                             )
+                                            { this.state.insight + event.target.value }
                                         })
                                         }
 
@@ -80,19 +75,3 @@ export default class Personality extends Component {
 
     }
 }
-
-
-{/* <div>
-    <form onSubmit={this.handleSubmit} className='form'>
-
-        <h3>I rather: </h3>
-        <RadioButtonGroup name="q1" defaultSelected="not_light" onChange={this.handleChange}>
-
-            <RadioButton
-                value="Do Yoga"
-                label="Do Yoga"
-                style={styles.radioButton}
-
-            />
-            
-        </RadioButtonGroup> */}
