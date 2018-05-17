1055fa5d799a6045a8c41c88ab1cb21debcb1fa8
Tania Santamaria <tsantamaria.r@gmail.com>
2018-05-17 14:42:50 -0400

    test

--- client/components/Progress.js
+++ client/components/Progress.js
@@ -1,53 +1,66 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
-import {VictoryPie, VictoryAnimation, VictoryLabel} from 'victory'
+import { connect } from 'react-redux'
+import { VictoryPie, VictoryAnimation, VictoryLabel } from 'victory'
 
 const getData = percent => {
   return [{ x: 1, y: percent }, { x: 2, y: 100 - percent }];
 }
 
+
+function timer() {
+  setTimeout(function () { alert(); }, 2000);
+}
+
+
+
+
+
+
 /**
  * COMPONENT
  */
 const Progress = props => {
+  const level = props.level + 1;
+  return (
 
-    return (
-      <div >
-        {props.levelledUp ? (<div>Level {props.level + 1}!</div>) : (<div />)}
-        <h1 className="total-progress">{props.name}</h1>
-        <svg viewBox="0 0 400 400" width="100%" height="100%">
-          <VictoryPie
-            standalone={false}
-            animate={{ duration: 1000 }}
-            width={400} height={400}
-            data={getData(props.progress)}
-            innerRadius={props.category ? 0 : 120}
-            cornerRadius={25}
-            labels={() => null}
-            style={{
-              data: { fill: (d) => {
-                  const color = d.y > 30 ? '#DBCFA6' : '#B33B33'
-                  return d.x === 1 ? color : 'transparent'
-                }
+    <div >
+      {props.levelledUp ? (timer(setTimeout(function () { alert('Level ' + level + '!') }, 2000))) : null}
+      {/* {props.levelledUp ? (<div>Level {props.level + 1}!</div>) : (<div />)} */}
+      <h1 className="total-progress">{props.name}</h1>
+      <svg viewBox="0 0 400 400" width="100%" height="100%">
+        <VictoryPie
+          standalone={false}
+          animate={{ duration: 1000 }}
+          width={400} height={400}
+          data={getData(props.progress)}
+          innerRadius={props.category ? 0 : 120}
+          cornerRadius={25}
+          labels={() => null}
+          style={{
+            data: {
+              fill: (d) => {
+                const color = d.y > 30 ? '#DBCFA6' : '#B33B33'
+                return d.x === 1 ? color : 'transparent'
               }
-            }}
-          />
-          <VictoryAnimation duration={1000} data={props}>
-            {() => {
-              return (
-                <VictoryLabel
-                  textAnchor="middle" verticalAnchor="middle"
-                  x={200} y={200}
-                  text={`${Math.round(props.progress)}%`}
-                  style={{ fontSize: 45 }}
-                />
-              )
-            }}
-          </VictoryAnimation>
-        </svg>
-      </div>
-    )
+            }
+          }}
+        />
+        <VictoryAnimation duration={1000} data={props}>
+          {() => {
+            return (
+              <VictoryLabel
+                textAnchor="middle" verticalAnchor="middle"
+                x={200} y={200}
+                text={`${Math.round(props.progress)}%`}
+                style={{ fontSize: 45 }}
+              />
+            )
+          }}
+        </VictoryAnimation>
+      </svg>
+    </div>
+  )
 }
 
 /**
